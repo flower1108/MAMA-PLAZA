1f8060def7ca0c5fd29a06a0f4f503b95fc9d22c
flower1108 <129576720+flower1108@users.noreply.github.com>
2023-06-11 14:59:30 +0000

    add

--- db/migrate/20230607091414_create_categories.rb
+++ db/migrate/20230607091414_create_categories.rb
@@ -2,7 +2,7 @@ class CreateCategories < ActiveRecord::Migration[6.1]
   def change
     create_table :categories do |t|
 
-      t.string :cateegory
+      t.string :category
       t.timestamps
     end
   end
--- db/migrate/20230609083601_add_child_information_to_user.rb
+++
@@ -1,5 +0,0 @@
-class AddChildInformationToUser < ActiveRecord::Migration[6.1]
-  def change
-    add_column :users, :child_information, :text
-  end
-end
