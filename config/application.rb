c7addf8845b9a616f644827f6b7e3d13737a2cd2
flower1108 <129576720+flower1108@users.noreply.github.com>
2023-06-13 02:31:23 +0000

    [Add]投稿一覧のレイアウト

--- config/application.rb
+++ config/application.rb
@@ -19,4 +19,8 @@ module MamaPlaza
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
   end
+
+  #日本時間の設定
+  config.i18n.default_locale = :ja
+  config.time_zone = 'Tokyo'
 end
