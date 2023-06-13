4e0ee389fc7791926e3332ed1a9bc450adc83d34
flower1108 <129576720+flower1108@users.noreply.github.com>
2023-06-13 11:34:04 +0000

    [Add]デフォルトプロフィール画像追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,14 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
 
+  before_create :default_image
+
+  def default_image
+  if !self.profile_image.attached?
+    self.profile_image.attach(io: File.open(Rails.root.join('app', 'assets', 'images', 'profile-noimage.png')), filename: 'profile-noimage.png', content_type: 'image/png')
+  end
+end
+  #ゲストログイン
   def self.guest
     find_or_create_by!(email: 'guest@example.com') do |user|
       user.password = SecureRandom.urlsafe_base64
