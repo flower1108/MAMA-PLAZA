6956653c4ac495243f6ca2e44e53ad2045a53486
flower1108 <129576720+flower1108@users.noreply.github.com>
2023-06-16 10:55:58 +0000

    [Add]admins/post

--- app/controllers/admins/users_controller.rb
+++ app/controllers/admins/users_controller.rb
@@ -1,7 +1,18 @@
 class Admins::UsersController < ApplicationController
+
   def show
+    @user = User.find(params[:id])
+    @posts = @user.post
   end
 
   def index
+    @user = User.all
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :child_information, :introduction, :profile_image)
+  end
+
 end
