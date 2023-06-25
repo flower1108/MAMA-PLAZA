eeda76143a53ed2d8ce68e7c648fd31eab6865a2
flower1108 <129576720+flower1108@users.noreply.github.com>
2023-06-25 14:48:44 +0000

    [Add]ログイン後のリダイレクト設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,11 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
-    user_path(current_user)
+    if resource.is_a?(Admin)
+      root_path
+    else
+      user_path(current_user)
+    end
   end
 
   private
