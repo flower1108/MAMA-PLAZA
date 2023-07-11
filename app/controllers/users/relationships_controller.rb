2fd19ca7ecb943f6ef0188a0a085a80cf76c3043
flower1108 <129576720+flower1108@users.noreply.github.com>
2023-07-11 12:37:28 +0000

    [Add]follow非同期化

--- app/controllers/users/relationships_controller.rb
+++ app/controllers/users/relationships_controller.rb
@@ -3,13 +3,15 @@ class Users::RelationshipsController < ApplicationController
   #フォローする時
   def create
     current_user.follow(params[:user_id])
-    redirect_to request.referer
+    @user = User.find(params[:user_id])
+    #redirect_to request.referer
   end
 
   #フォローを外す時
   def destroy
     current_user.unfollow(params[:user_id])
-    redirect_to request.referer
+    @user = User.find(params[:user_id])
+    #redirect_to request.referer
   end
 
 end
